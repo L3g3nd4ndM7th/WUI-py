ad68b77c70304bec1a48060b39b05ccf05c402cb
L3g3nd4ndM7th <134120635+L3g3nd4ndM7th@users.noreply.github.com>
2024-06-07 16:07:48 -0400

    Delete source directory

--- source/classGameMap.py
+++
@@ -1,7 +0,0 @@
-class GameMap:
-    def __init__(self, name="Default Map", height=10, width=10, start=[0, 0], cells=[]):
-        self.name = name
-        self.height = height
-        self.width = width
-        self.start = start
-        self.cells = cells
